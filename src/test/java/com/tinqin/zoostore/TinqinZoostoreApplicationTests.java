2e0116aecf7dc23a08505d1c593c536002aadcc4
Plamen Simeonov <haibusa1991@gmail.com>
2023-07-13 09:04:12 +0300

    added integration test place holder

--- src/test/java/com/tinqin/zoostore/TinqinZoostoreApplicationTests.java
+++ src/test/java/com/tinqin/zoostore/TinqinZoostoreApplicationTests.java
@@ -1,13 +1,38 @@
 package com.tinqin.zoostore;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.awt.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(value = "/test.yaml")
 class TinqinZoostoreApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void testGetVendor() throws Exception {
+		ResultActions perform = mockMvc.perform(get("/items"));
+		System.out.println();
+
 	}
 
 }
